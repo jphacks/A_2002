1daced4a0e61a27a32bc3482301e496f1041bf50
matsuopassion <matsuopassion@gmail.com>
2020-10-31 14:50:32 +0900

    SpringBootプロジェクト新規作成

---
+++ jphacks_a2002/src/main/java/jphacks_a2002/JphacksA2002Application.java
@@ -0,0 +1,13 @@
+package jphacks_a2002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JphacksA2002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JphacksA2002Application.class, args);
+	}
+
+}
---
+++ jphacks_a2002/src/test/java/jphacks_a2002/JphacksA2002ApplicationTests.java
@@ -0,0 +1,13 @@
+package jphacks_a2002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JphacksA2002ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
