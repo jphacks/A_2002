230283ede380e9a7c46fb799a9c89c588b61f39b
matsuopassion <matsuopassion@gmail.com>
2020-11-03 17:20:39 +0900

    MangaService作成途中多分もうちょいで完成

--- jphacks_a2002/src/main/java/jphacks_a2002/manga/MangaService.java
+++ jphacks_a2002/src/main/java/jphacks_a2002/manga/MangaService.java
@@ -1,6 +1,7 @@
 package jphacks_a2002.manga;
 
 import java.util.Collections;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,7 +30,26 @@ public class MangaService {
 	}
 
 	//新規に作成された漫画をデータベースに登録（一応登録件数を返す）
+	//新規漫画作成時に必要なテーマなどはthemeパッケージにて記述
 	public int addNewManga(MangaData mangaData) {
-		return mangaRepository.InsertManga(mangaData);
+		//漫画ごとの一意なIDの割り振りは話し合ってからRepositoryかこっちに記述
+		return mangaRepository.insertManga(mangaData);
+	}
+
+	//コマ追加画面遷移時に未完成な漫画のデータを一つ取得する
+	public MangaData getRandomManga() {
+		//未完成漫画のリストを作成
+		MangaEntity undoneMangaEntity = mangaRepository.selectUndoneAll();
+		int randGetIndex = new Random().randInt(undoneMangaEntity.size());
+		return undoneMangaEntity.get(randGetIndex);
+	}
+
+	//ここの処理については明日先生に聞くかも
+	//コントローラでコマテーブルの情報を作成した後こっちを呼び出して欲しい
+	//コマのパッケージの方でコマIDをコントローラに返すように記述する
+	//正直このやり方が最良かはよくわかんね
+	public int addNewFrame(int mangaId,int frameId) {
+		//リポジトリ側でコマFlagみたいなやつ1進めるように作っとてん
+		return mangaRepository.insertFrame(mangaId,frameId);
 	}
 }
