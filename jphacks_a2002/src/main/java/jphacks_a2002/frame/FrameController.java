c8fecbd9742fdfded799a9b54a1ca81b89705478
ウェムラ <@192.168.0.14>
2020-11-04 01:20:59 +0900

    漫画作成確定のコントローラー

---
+++ jphacks_a2002/src/main/java/jphacks_a2002/frame/FrameController.java
@@ -0,0 +1,42 @@
+package jphacks_a2002.frame;
+
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import jphacks_a2002.manga.MangaData;
+
+
+/**
+ * HTMLから受け取ったパラメータを扱うクラス
+ * @author 植村
+ *
+ */
+@Controller
+public class FrameController {
+
+	@Autowired
+	FrameService frameService;
+
+
+	/**
+	 * 漫画作成を確定する
+	 * @param form
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/createManga/cofirm")
+	public String confirmCreateManga(@ModelAttribute @Validated FrameData form, Principal principal, Model model) {
+		FrameData frameData = new FrameData();
+		MangaData mangaData = frameService.addNewFrame(frameData);
+		model.addAttribute("mangaData",mangaData);
+		return "top/join";
+	}
+
+}
