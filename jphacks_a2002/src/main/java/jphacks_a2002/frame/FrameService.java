ba95f559f596ddbe5ff66ef67d3b758c84620213
matsuopassion <matsuopassion@gmail.com>
2020-11-03 18:09:22 +0900

    FrameServiceの作成

--- jphacks_a2002/src/main/java/jphacks_a2002/frame/FrameService.java
+++ jphacks_a2002/src/main/java/jphacks_a2002/frame/FrameService.java
@@ -1,5 +1,6 @@
 package jphacks_a2002.frame;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -7,5 +8,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class FrameService {
 
-	
+	@Autowired
+	FrameRepositrory frameRepository;
+
+	//コマの追加時に呼び出される
+	//MangaControllerから呼び出される予定だが、ちょっと微妙かもと思っている
+	//返り値はコマID
+	public int AddNewFrame(FrameData frameData) {
+		return frameRepository.InsertFrame(frameData);
+	}
+
 }
