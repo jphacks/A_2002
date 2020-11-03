1d34c719d5da49caa69ee6ddfb6421e621e763e2
matsuopassion <matsuopassion@gmail.com>
2020-11-03 17:49:15 +0900

    Frameパッケージの追加

---
+++ jphacks_a2002/src/main/java/jphacks_a2002/frame/FrameService.java
@@ -0,0 +1,11 @@
+package jphacks_a2002.frame;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
+public class FrameService {
+
+	
+}
