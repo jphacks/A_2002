b919b820d67845f9cdb3a868af7a2ab1fb22c4e0
matsuopassion <matsuopassion@gmail.com>
2020-11-06 22:09:31 +0900

    indexはみ出ないようにした

--- jphacks_a2002/src/main/java/jphacks_a2002/manga/MangaController.java
+++ jphacks_a2002/src/main/java/jphacks_a2002/manga/MangaController.java
@@ -77,7 +77,7 @@ public class MangaController {
 	public String getJoinDisplay(Principal principal, Model model) {
 		MangaData mangaData = mangaService.getRandomManga();
 		model.addAttribute("mangaData",mangaData);
-		model.addAttribute("beforePath", mangaData.getFramelist().get(mangaData.status));
+		model.addAttribute("beforePath", mangaData.getFramelist().get(mangaData.status - 1 ));
 		return "/join";
 	}
 
