32d488136a5fd8365b56aa3f93c7c6d6ac54ff5b
tatus <tatus@192.168.11.8>
2020-11-07 03:18:28 +0900

    いったん

--- jphacks_a2002/src/main/java/jphacks_a2002/frame/FrameService.java
+++ jphacks_a2002/src/main/java/jphacks_a2002/frame/FrameService.java
@@ -12,21 +12,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jphacks_a2002.manga.MangaData;
+import jphacks_a2002.manga.MangaService;
+
 @Transactional
 @Service
 public class FrameService {
 
 	@Autowired
 	FrameRepository frameRepository;
+	@Autowired
+	MangaService mangaService;
 
 	//コマの追加時に呼び出される
 	//MangaControllerから呼び出される予定だが、ちょっと微妙かもと思っている
 	//返り値はコマID
 	//このやり方だとFrameControllerがいらないのよな
-	public int addNewFrame(FrameForm frameForm) throws IOException {
+	public int addNewFrame(FrameForm frameForm,MangaData mangaData) throws IOException {
 		int frameNameLast = frameRepository.getLastFrameID();
 		this.writeImgFile(frameForm.getImageData(),Integer.toString(frameNameLast)+".png");
-		return frameRepository.insertOneFrame(this.formToData(frameForm));
+		return frameRepository.insertOneFrame(this.formToData(frameForm,mangaData));
 	}
 
 	private void writeImgFile(String imgStr,String imgName) throws IOException {
@@ -35,16 +40,16 @@ public class FrameService {
 		Files.write(destinationFile, decodedImg);
 	}
 
-	public FrameData formToData(FrameForm frameForm) {
+	public FrameData formToData(FrameForm frameForm,MangaData mangaData) {
 		Date date = new Date();
 		//FrameIDはデータベースで採番するためここで割り振らない
 		FrameData data = new FrameData();
 		data.setCreater(frameForm.getCreater());
 		data.setPath(frameForm.getPath());
 		data.setCreateDate(date);
-		data.setMangaID(frameForm.getMangaID());
+		data.setMangaID(mangaData.getMangaID());
 		//これ画面から渡されるやつに1足すって処理でいいかわかんねえ
-		data.setFrameID(frameForm.getFrameNo() + 1 );
+		data.setFrameID(mangaData.getFrameNo() + 1 );
 		return data;
 	}
 }
